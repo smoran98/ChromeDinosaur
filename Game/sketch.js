e0b44d89b836e3b361544b80f033041bf1534220
smoran98 <g00338607@gmit.ie>
2020-03-26 20:36:50 +0000

    Speech working

--- Game/sketch.js
+++ Game/sketch.js
@@ -1,10 +1,15 @@
-let dinosaur
-let dImag
-let tImag
-let bImag
+let dinosaur;
+let dImag;
+let tImag;
+let bImag;
 let ballers = [];
+let soundClass;
 
 function preload() {
+    const options = {
+      probabilityThreshold: 0.95
+    };
+    soundClass = ml5.soundClass('SpeechCommands18w', options);
     dImg = loadImage('dinosaur.png');
     tImg = loadImage('baller.png');
     bImg = loadImage('background.png');
@@ -13,6 +18,18 @@ function preload() {
 function setup() {
   createCanvas(800, 450);
   dinosaur = new dino();
+  soundClass.classify(gotCommand);
+}
+
+function gotCommand(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  console.log[0](results[0].label, results[0].confidence);
+  if (results[0].label == 'up') {
+    dino.jump();
+  }
+
 }
 
 function keyPressed() {
